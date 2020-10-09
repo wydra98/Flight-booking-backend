cf2c369b58894a3818df2f446c674cba0584b708
Adrian Wydra <wydra.adrian98@gmail.com>
2020-10-09 17:37:18 +0200

    Deleting end-to-end tests.

--- src/main/java/flight_booking/backend/BackendApplication.java
+++ src/main/java/flight_booking/backend/BackendApplication.java
@@ -2,9 +2,11 @@ package flight_booking.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestOperations;
 
 
 @SpringBootApplication
--- src/test/java/flight_booking/backend/end_to_end_tests/AirportControllerTest.java
+++
@@ -1,4 +0,0 @@
-package flight_booking.backend.end_to_end_tests;
-
-public class AirportControllerTest {
-}
